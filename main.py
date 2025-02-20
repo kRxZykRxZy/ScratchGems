54ca4648841eef08c4b4b8646b48c503f6ef3172
kRxZykRxZy <Hi1992@yogirt.com>
2025-02-20 13:48:54 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import threading
+
+def start_server():
+    os.system("python base.py")
+
+def start_api():
+    os.system("python api.py")
+
+server = threading.Thread(target=start_server)
+api = threading.Thread(target=start_api)
+
+server.start()
+api.start()
