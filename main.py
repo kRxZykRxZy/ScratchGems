1c3af833b6a7dc2202a976120372cb1380bbad6f
kRxZykRxZy <Hi1992@yogirt.com>
2025-02-21 11:37:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import os
-import threading
-import subprocess
 
-subprocess.run(["sh", "Terminal.sh"])  # To install all packages. 
-
-def start_base():
-    os.system("python base.py")
-
-def start_api():
-    os.system("python api.py")
-
-server = threading.Thread(target=start_server)
-api = threading.Thread(target=start_api)
-
-base.start() # Starts base
-api.start() # Starts API
