83b526fb504be7c6f09518c9b6e0e9f07a75a1a4
kRxZykRxZy <Hi1992@yogirt.com>
2025-02-21 11:35:44 +0000

    Update base.py

--- base.py
+++ base.py
@@ -2,8 +2,19 @@ import scratchattach as sa
 import time
 import random
 import string
+import json
 
-# Initialize mining and logging
+# Open and read the JSON file
+with open('scratchgems.json', 'r') as file:
+    data = json.load(file)
+    
+    people = data
+
+print(data)  # Prints the JSON content as a dictionary
+
+
+
+ # Initialize mining and logging
 miner = []
 log = []
 
@@ -14,9 +25,12 @@ def generate_random_letters(length=10):
 random_letters = generate_random_letters()
 miner.append(random_letters)
 
-session = sa.login("Username", "Password")  # Replace with your credentials
+session = sa.login("USERNAME", "PASSWORD")  # Replace with your credentials
 cloud = session.connect_cloud("1134723891")  # Replace with your project ID
 client = cloud.requests()
+
+with open('scratchgems.json', 'w') as file:
+    json.dump(people, indent = 4)
 
 @client.request
 def ping():
@@ -26,23 +40,42 @@ def ping():
 @client.request
 def gift_receive(argument1):
     print(f"Gift received request: {argument1}")
-    if argument1 in log:
-        amount = log.pop(log.index(argument1) + 1)
-        log.remove(argument1)
-        return amount
+    return people[people.index(argument1) + 1]
+    with open('scratchgems.json', 'w') as file:
+        json.dump(people, indent = 4)
+
 
 @client.request
-def gift_send(argument1, argument2):
+def gift_send(argument1, argument2, argument3):
     print(f"Gift {argument1} to {argument2}")
-    log.append(argument2)
-    log.append(argument1)
-
+    if argument2 in people:
+        bal = people.index(argument3) + 1
+        people[bal] = people[bal] - argument1
+        balance1 = people.index(argument2) + 1
+        people[balance1] = people[balance1] + argument1
+        with open('scratchgems.json', 'w') as file:
+            json.dump(people, indent = 4)
+    
 @client.request
 def mine(argument1):
     print(f"Mining Code: {argument1}")
     if argument1 in miner:
+        client.send(message)
         return "100"
+        
 
+@client.request
+def new(argument1):
+    print(f"New: {argument1}")
+    if argument1 in people:
+        true = false
+    elif 10 == 10:
+        people.append(argument1)
+        people.append("100")
+        return "new"
+        with open('scratchgems.json', 'w') as file:
+            json.dump(people, indent = 4)
+    
 @client.event
 def on_ready():
     print("Request handler is running")
