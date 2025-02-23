9fbe7b4be6dfcbfba6d51b09a7f61039c26ffc40
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-02-23 09:16:15 +0000

    Update API.py

--- API.py
+++ API.py
@@ -8,25 +8,24 @@ with open('scratchgems.json', 'r') as file:
     balances = data
 
 # API coming soon, still in coding progress. 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Extract username from the query parameter
+def get_username():
+    # Extract 'username' from the query parameters
     username = request.args.get('username')
+  
 
 balance = balances.index(username) + 1
-if username:
+
+if username: 
     with open('html_load.json', 'w') as file:
-            json.dump(username, indent = 0)
+        json.dump(username, indent = 0)
     with open('balance_html.json', 'w') as file:
-            json.dump(balance, indent = 0)
+        json.dump(balance, indent = 0)
 else:
-    with open('html_load.json', 'w') as file:
-            json.dump('FAILED TO LOAD' , indent = 0)
-
-    with open('balance_html.json', 'w') as file:
-            json.dump(FAILED TO LOAD, indent = 0)
-
+    os.shutdown
 if __name__ == '__main__':
     app.run(debug=True)
